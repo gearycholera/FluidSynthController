b32cb23cd934a7f82a8ce5e14e90c7cdaae50382
girikalra <girikalra@gmail.com>
2018-05-20 02:16:54 -0700

    light cleanup

--- client/src/components/Channel.jsx
+++ client/src/components/Channel.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import instruments from './../../../options.js';
 
 const Channel = (props) => {
+  let ch = props.ch + 1;
+
   return (
     <div>
-      <span>channel {((props.ch + 1) < 10 ? '0' : '') + (props.ch + 1)} </span> <select id={props.ch} onChange={props.handleChange}>
-          {instruments.map((e, key) => {
-              return <option key={key} value={e.value}>{e.name}</option>;
-          })}
+      <span>channel {(ch < 10 ? '0' : '') + ch}</span> <select id={props.ch} onChange={props.handleChange}>
+        {instruments.map((e, key) => {return <option key={key} value={e.value}>{e.name}</option>})}
       </select>
     </div>
   )
